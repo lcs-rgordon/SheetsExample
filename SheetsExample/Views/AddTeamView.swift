de9590178027fdbbb3de1a160a935ff981a5e41d
Russell Gordon <github@russellgordon.ca>
2022-05-11 06:02:10 -0400

    Connected team list view and add team view, using boolean to track whether add team view shows.

--- SheetsExample/Views/AddTeamView.swift
+++ SheetsExample/Views/AddTeamView.swift
@@ -11,6 +11,9 @@ struct AddTeamView: View {
     
     // MARK: Stored properties
     
+    // A derived value; this is connected to the source of truth on TeamListView
+    @Binding var isAddTeamViewShowing: Bool
+    
     // MARK: Computed properties
     var body: some View {
         
@@ -23,6 +26,22 @@ struct AddTeamView: View {
                 Spacer()
             }
             .navigationTitle("Add Team")
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    
+                    Button(action: {
+                        
+                        // Hide the view by setting the boolean back to false
+                        isAddTeamViewShowing = false
+                        
+                    }, label: {
+                        
+                        Text("Done")
+                        
+                    })
+                    
+                }
+            }
             
         }
         
@@ -31,6 +50,6 @@ struct AddTeamView: View {
 
 struct AddTeamView_Previews: PreviewProvider {
     static var previews: some View {
-        AddTeamView()
+        AddTeamView(isAddTeamViewShowing: .constant(true))
     }
 }
--- SheetsExample/Views/TeamListView.swift
+++ SheetsExample/Views/TeamListView.swift
@@ -11,16 +11,41 @@ struct TeamListView: View {
     
     // MARK: Stored properties
     
+    // Should the add team interface be shown?
+    // The source of truth (value is created and stored here)
+    @State var isAddTeamViewShowing = false
+    
     // MARK: Computed properties
     var body: some View {
         
         NavigationView {
+            
             List {
                 Text("Liverpool")
                 Text("Manchester City")
                 Text("Chelsea")
             }
             .navigationTitle("Teams")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    
+                    Button(action: {
+                        
+                        // Show the add team view by setting the boolean to true
+                        isAddTeamViewShowing = true
+                        
+                    }, label: {
+                        
+                        Text("Add New Team")
+                        
+                    })
+                    
+                }
+            }
+            .sheet(isPresented: $isAddTeamViewShowing) {
+                AddTeamView(isAddTeamViewShowing: $isAddTeamViewShowing)
+            }
+
         }
         
     }
