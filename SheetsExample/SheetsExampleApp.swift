0aaaf1bd595ebcab4cb1b4829ac54ae81c7068e4
Russell Gordon <github@russellgordon.ca>
2022-05-11 05:47:42 -0400

    Initial Commit

---
+++ SheetsExample/SheetsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetsExampleApp.swift
+//  SheetsExample
+//
+//  Created by Russell Gordon on 2022-05-11.
+//
+
+import SwiftUI
+
+@main
+struct SheetsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
